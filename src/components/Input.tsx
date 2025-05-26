11b53c872587d749708d76f0e851fa47f1a685c3
Alireza-ghate <alireza.ghate.jd@gmail.com>
2025-05-26 19:12:18 +0330

    fix styles in Input component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -4,7 +4,7 @@ function Input(props: InputProps) {
   return (
     <input
       {...props}
-      className={`border-gray-300 border bg-white focus:outline-none focus:ring-1 focus:ring-gray-400 rounded-md px-4 py-2 ${
+      className={`border border-gray-300 outline-none focus:border-gray-500 bg-white rounded-md px-4 py-2 ${
         props.className || ""
       }`}
     />
